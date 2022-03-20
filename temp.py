ed878aa489d751a8e420a094ef00b83f83a124c2
Jason Robinson <jmrobinsonma@gmail.com>
2022-03-20 10:38:07 -0400

    initial commit

---
+++ temp.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+from plyer import notification
+#import json
+
+
+def temp(args = '-jf'):
+	cmd = 'sensors'
+	fetch = subprocess.Popen([cmd,args],stdout=subprocess.PIPE)
+	output = str(fetch.communicate())
+	output = output.split('\n')
+	output = output[0].split('\"')
+	res = []
+	
+	for line in output:
+		res.append(line.split('\\')[0])
+
+	msg = f"""
+{res[21].split(',')[0]}{res[24].split(',')[0]}
+{res[5]}{res[10].split(',')[0]}
+{res[25].split(',')[0]}{res[28].split(',')[0]}
+{res[29].split(',')[0]}{res[32].split(',')[0]}
+"""
+	notification.notify(
+		title = "System Temp",
+		message = msg,
+		app_icon = "Paomedia-Small-N-Flat-Dashboard-alt.ico",
+		timeout = 5,
+	)
+
+
+
+temp()
+
+
